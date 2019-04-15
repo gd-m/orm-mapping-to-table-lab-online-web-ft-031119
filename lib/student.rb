88e7173af69f9074700f1a1ea4bfaceeaba48570
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-04-15 18:36:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,8 +32,9 @@ class Student
     INSERT INTO students (name, grade) VALUES (?,?)
     SQL
 
-    DB[:conn].execute(sql,self.name,self.grade)
+    DB[:conn].execute(sql, self.name, self.grade)
   end
+  
 
 
 
